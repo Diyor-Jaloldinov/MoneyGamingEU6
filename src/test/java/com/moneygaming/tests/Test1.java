001d2528b68ab9478e82bb65bf2822c7abaeb523
Diyor-Jaloldinov <diyor.jaloldinov@gmail.com>
2021-09-18 17:06:41 +0200

    added some new test methods

--- src/test/java/com/moneygaming/tests/Test1.java
+++ src/test/java/com/moneygaming/tests/Test1.java
@@ -1,14 +1,30 @@
 package com.moneygaming.tests;
 
+import com.moneygaming.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Test1 {
 
+    WebDriver driver;
 
     @BeforeTest
     public void setUP(){
+        driver = WebDriverFactory.getDriver("edge");
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test1(){
+
+    }
 
 
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 
 
